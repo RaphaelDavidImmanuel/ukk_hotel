1b227c64ddc7e2637c1781173cf90fa12c768f02
Raphael Immanuel <raphaeldavid1711.ri@gmail.com>
2022-09-19 21:44:53 +0700

    migration

---
+++ app/Models/fasilitaskamar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class fasilitaskamar extends Model
+{
+    protected $table = 'fasilitaskamars';
+    protected $guarded = [''];
+
+    // protected function fasilitaskamar(){
+    //     return $this->belongsTo(fasilitaskamar::class);
+    // }
+}
---
+++ database/migrations/2022_09_19_143819_create_pemesanans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePemesanansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->integer("nomor_pemesanan");
+            $table->string("username_id");
+            $table->foreignId("kategori_id");
+            $table->date("check_in");
+            $table->date("check_out");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+}
